3c9c210a3774bf4a8e7cf4846eacb02550233dd2
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-10-16 21:48:18 +0530

    added layout

--- Web-app/app.py
+++ Web-app/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/add")
+def addqn():
+    return render_template("addqn.html")
+
+
 if __name__ == '__main__':
     app.run()
