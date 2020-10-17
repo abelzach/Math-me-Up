c7d3f41fa269c7c72efa616b3fa2bb57e0f04ed5
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-10-17 20:02:18 +0530

    Front-end works

--- Web-app/app.py
+++ Web-app/app.py
@@ -1,21 +1,23 @@
 from flask import Flask, render_template
 from cs50 import SQL
+from helper import url
 
 
 app = Flask(__name__)
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 db = SQL("sqlite:///questions.db")
 
-
-@app.route("/add")
-def addqn():
-    return render_template("addqn.html")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/add")
+def addqn():
+    return render_template("addqn.html")
 
 
 if __name__ == '__main__':
