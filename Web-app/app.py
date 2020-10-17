206a10b7980da88b8599005da5d3ef0d9ed37ccd
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-10-18 00:01:17 +0530

    Major advances

--- Web-app/app.py
+++ Web-app/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 from cs50 import SQL
-from helper import url
+import random
+import requests
+from helper import url, Device_ID, API_KEY
 
 
 app = Flask(__name__)
@@ -12,8 +14,15 @@ db = SQL("sqlite:///questions.db")
 
 @app.route("/", methods = ["GET" , "POST"])
 def index():
+    n = str(random.randint(0,100))
+    response = requests.get(url+n)
     if request.method == "GET":
-        return render_template("index.html")
+        return render_template("index.html", Device_ID=Device_ID, API_KEY=API_KEY, fact=response.text)
+    else:
+        dif = request.form.get("difficulty")
+        tpc = request.form.get("topic")
+        Qndata = db.execute("SELECT * FROM questions WHERE topic = :tpc AND difficulty = :dif", tpc=tpc, dif=dif)
+        return render_template("index.html", Qndata=Qndata, Device_ID=Device_ID, API_KEY=API_KEY, fact=response.text)
 
 
 @app.route("/add", methods = ["GET" , "POST"])
--- Web-app/helper.py
+++ Web-app/helper.py
@@ -1,1 +1,3 @@
 url = "http://numbersapi.com/"
+Device_ID = "Your device-id"
+API_KEY = "your api-key"
