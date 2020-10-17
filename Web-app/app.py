ac5ff81ea478c75eff058439ec79d89cb993fb14
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-10-17 12:27:28 +0530

    rework1

--- Web-app/app.py
+++ Web-app/app.py
@@ -1,40 +1,20 @@
 from flask import Flask, render_template
-import sqlite3
-import os
+from cs50 import SQL
 
-currentdirectory = os.path.direname(os.path.abspath(__file__))
 
 app = Flask(__name__)
-         
 
-@app.route("/add", methods = ["POST"])
-def question():
-    ques = request.form["ques"]
-    ans = request.form["ans"]
-    level = request.form["level"]
-    maths = request.form["maths"]
-    connection = sqlite3.connect(currentdirectory = "\questions.db")
-    cursor = connection.cursor()
-    query1 = "INSERT INTO questions VALUES({q},{a},{l},{m})".format(q = ques, a = ans, l = level, m = maths)
-    cursor.execute(query1)
-    connection.commit()
+
+db = SQL("sqlite:///questions.db")
+
+
+@app.route("/add")
 def addqn():
     return render_template("addqn.html")
 
-@app.route("/", methods = ["GET"])
+@app.route("/")
 def index():
-    try:
-        if request.method == "GET":
-            level = request.form["level"]
-            maths = request.form["maths"]
-            connection = sqlite3.connect(currentdirectory = "\questions.db")
-        cursor = connection.cursor()
-        query1 = "SELECT ques from questions WHERE level = {l}".format(l = level)
-        cursor.execute(query1)
-        result = result.fetchall()[0][0]
-        return render_template("index.html",ques = result)
-    except:
-        return render_template("index.html", ques = "")
+    return render_template("index.html")
 
 
 
