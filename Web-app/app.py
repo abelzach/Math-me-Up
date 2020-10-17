22b617083b784686a37cf0a4073fbfb0ed49cf16
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-10-17 20:21:17 +0530

    DB updates

--- Web-app/app.py
+++ Web-app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from cs50 import SQL
 from helper import url
 
@@ -10,15 +10,23 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 db = SQL("sqlite:///questions.db")
 
 
-@app.route("/")
+@app.route("/", methods = ["GET" , "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "GET":
+        return render_template("index.html")
 
 
-@app.route("/add")
+@app.route("/add", methods = ["GET" , "POST"])
 def addqn():
-    return render_template("addqn.html")
-
+    if request.method == "GET":
+        return render_template("addqn.html")
+    else:
+        qstn = request.form.get("question")
+        tl = request.form.get("time")
+        tpc = request.form.get("topic")
+        dif = request.form.get("difficulty")
+        db.execute("INSERT INTO questions(qn, topic, difficulty, timelimit) VALUES(:qstn, :tpc, :dif, :tl)", qstn=qstn, tpc=tpc, dif=dif, tl=tl)
+        return redirect("/")
 
 if __name__ == '__main__':
     app.run()
